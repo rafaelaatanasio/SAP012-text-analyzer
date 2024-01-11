79fd294af40af42231e768ded1bbdaf1b5efcb78
Rafaela Atanasio <rah.jd@hotmail.com>
2024-01-10 22:06:56 -0300

    Updates

---
+++ analyzer.js
@@ -0,0 +1,49 @@
+const analyzer = {
+  getWordCount: (text) => {
+    //Esta função deve retornar a contagem de palavras encontradas no parâmetro `text` do tipo `string`.
+    const palavras = text.split(' ');
+    return palavras.length;
+  },
+
+  getCharacterCount: (text) => {
+    //Esta função deve retornar a contagem de caracteres que estão no parâmetro `text` do tipo `string`.
+    return text.length;
+  },
+
+  getCharacterCountExcludingSpaces: (text) => {
+    //Esta função deve retornar a recuperação de caracteres excluindo espaços e sinais de pontuação que se encontram no parâmetro `text` do tipo `string`.
+    const cleanText = text.replace(/[^\w\s]/g, "").replace(/\s+/g, "");
+    return cleanText.length;
+  },
+
+  getAverageWordLength: (text) => {
+    //Esta função deve retornar o comprimento médio das palavras que estão no parâmetro `text` do tipo `string`.
+    const words = text.trim().split(/\s+/);
+    const characterTotal = words.reduce((acc, word) => acc + word.length, 0);
+    const average = characterTotal / words.length || 0;
+    return parseFloat(average.toFixed(2));
+  },
+
+  getNumberCount: (text) => {
+    //Esta função deve retornar quantos números estão no parâmetro `text` do tipo `string`.
+    const isThatANum = text.match(/\b\d+(\.\d+)?\b/g);
+    return isThatANum ? isThatANum.length : 0;
+  },
+
+  getNumberSum: (text) => {
+    //Esta função deve retornar a soma de todos os números que estão no parâmetro `text` do tipo `string`.
+    const numberSum = text.match(/\b\d+(\.\d+)?\b/g);
+    if (numberSum) {
+      let total = 0;
+      for (let i = 0; i < numberSum.length; i++) {
+        total += Number(numberSum[i]);
+      }
+      return total;
+    } else {
+      return 0;
+    }
+  },
+
+};
+
+export default analyzer;
---
+++ index.js
@@ -0,0 +1,38 @@
+import analyzer from './analyzer.js';
+
+//Elementos do DOM com os atributos para exibir, somar e contar.
+const textarea = document.querySelector('[name="user-input"]');
+const button = document.getElementById("reset-button");
+const wordCount = document.querySelector('[data-testid="word-count"]');
+const characterCount = document.querySelector('[data-testid="character-count"]');
+const characterNoSpacesCount = document.querySelector('[data-testid="character-no-spaces-count"]');
+const numberCount = document.querySelector('[data-testid="number-count"]');
+const numberSum = document.querySelector('[data-testid="number-sum"]');
+const wordLengthAverage = document.querySelector('[data-testid="word-length-average"]');
+
+textarea.addEventListener('keyup', allMetricsUpdate);
+button.addEventListener("click", clearAllMetricsAndText);
+
+function clearAllMetricsAndText() { //Limpa e redefine o conteúdo quando clicado.
+  textarea.value = "";
+  wordCount.textContent = "Contagem de palavras: 0";
+  characterCount.textContent = "Contagem de caracteres: 0";
+  characterNoSpacesCount.textContent = "Contagem de caracteres excluindo espaços e sinais: 0";
+  numberCount.textContent = "Contagem de números: 0";
+  numberSum.textContent = "Soma total de números: 0";
+  wordLengthAverage.textContent = "Comprimento médio das palavras: 0";
+}
+
+function allMetricsUpdate() { //Função de digitar o texto. Métricas são atualizadas com base no texto inserido.
+  const text = textarea.value;
+  wordCount.textContent = "Contagem de palavras: " + analyzer.getWordCount(text);
+  characterCount.textContent = "Contagem de caracteres: " + analyzer.getCharacterCount(text);
+  characterNoSpacesCount.textContent = "Contagem de caracteres excluindo espaços e sinais: " + analyzer.getCharacterCountExcludingSpaces(text);
+  numberCount.textContent = "Contagem de números: " + analyzer.getNumberCount(text);
+  numberSum.textContent = "Soma total de números: " + analyzer.getNumberSum(text);
+  wordLengthAverage.textContent = "Comprimento médio das palavras: " + analyzer.getAverageWordLength(text);
+}
+
+
+
+
